c9027fac3457d18bce318dcd1b708723b05af55a
Wendy Kobayashi <iamalittleforest@gmail.com>
2021-05-09 15:00:39 -0700

    populate question array

--- index.js
+++ index.js
@@ -1,7 +1,60 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  {
+    type: "input",
+    name: "title",
+    message: "",
+  },
+  {
+    type: "input",
+    name: "description",
+    message: ""
+  },
+  {
+    type: "input",
+    name: "installation",
+    message: "",
+  },
+  {
+    type: "input",
+    name: "usage",
+    message: ""
+  },
+  {
+    type: "list",
+    name: "license",
+    message: "",
+    choices: []
+  },
+  {
+    type: "input",
+    name: "contributing",
+    message: ""
+  },
+  {
+    type: "input",
+    name: "tests",
+    message: ""
+  },
+  {
+    type: "input",
+    name: "questions",
+    message: ""
+  },
+  {
+    type: "input",
+    name: "username",
+    message: ""
+  },
+  {
+    type: "input",
+    name: "email",
+    message: ""
+  }];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
