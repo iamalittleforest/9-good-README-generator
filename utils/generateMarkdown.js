7d37de795bf6ed5cf96bef151adc802ff938d3ec
Wendy Kobayashi <iamalittleforest@gmail.com>
2021-05-10 20:25:59 -0700

    updated output format of generateMarkdown function

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -14,37 +14,38 @@ function renderLicenseSection(license) {}
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `
-    # ${data.title}
-    
-    ## Description
-    ${data.description}
+# ${data.title}
 
-    ## Table of Contents
-    * [Installation](#installation)
-    * [Usage](#usage)
-    * [License](#license)
-    * [Contributing](#contributing)
-    * [Tests](#tests)
-    * [Questions](#questions)
-    
-    ## Installation
-    ${data.installation}
+## Description
+${data.description}
 
-    ## Usage
-    ${data.usage}
+## Table of Contents
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#license)
+* [Contributing](#contributing)
+* [Tests](#tests)
+* [Questions](#questions)
 
-    ## License
-    This project is licensed under the ${data.license} License.
+## Installation
+${data.installation}
 
-    ## Contributing
-    ${data.contributing}
+## Usage
+${data.usage}
 
-    ## Tests
-    ${data.tests}
+## License
+This project is licensed under the ${data.license} License.
 
-    ## Questions
-    If you have any questions, contact me directly at ${data.email}. See more of my work at [${data.username}](https://github.com/${data.username}/).
+## Contributing
+${data.contributing}
+
+## Tests
+${data.tests}
+
+## Questions
+If you have any questions, contact me directly at ${data.email}. See more of my work by visiting my [GitHub Profile](https://github.com/${data.username}/).
 `;
 }
 
+// export generateMarkdown module
 module.exports = generateMarkdown;
