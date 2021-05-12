3dd1a344e334c42f1d58913993d87af5aebb49d6
Wendy Kobayashi <iamalittleforest@gmail.com>
2021-05-11 21:37:19 -0700

    update URL for GPLv3 to match other licenses

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -2,7 +2,7 @@
 const licenses = {
   "MIT": "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)",
   "Apache 2.0": "[![License: Apache 2.0](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)",
-  "GPLv3": "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)",
+  "GPLv3": "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://opensource.org/licenses/GPL-3.0)",
   "BSD 3": "[![License: BSD 3](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)"
 }
 
